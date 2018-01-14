dc15537faf834f0ae5a0fef68b3778d9bcc4d36c
JoaoFerreira100 <32512196+JoaoFerreira100@users.noreply.github.com>
2018-01-14 19:13:04 +0100

    login

--- zooop/projectzoop/zoop/forms.py
+++ zooop/projectzoop/zoop/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 from zoop.models import Post
 
 class UserRegistrationForm(UserCreationForm):
@@ -36,20 +37,51 @@ class UserRegistrationForm(UserCreationForm):
             user.save()
         return user
 
-class LoginForm(forms.Form):
-    user = forms.CharField(required=True, label=False)
-    password = forms.CharField(widget=forms.PasswordInput(), label=False)
+class LoginForm(AuthenticationForm):
+    username = forms.CharField( label=False)
+    password = forms.CharField(widget=forms.PasswordInput, label=False)
+
+    error_messages = {
+        'invalid_login': ("Please enter a correct %(username)s and password. "
+                           "Note that both fields may be case-sensitive."),
+        'inactive': ("This account is inactive."),
+    }
 
     class Meta:
         model = User
-        fields = ("user", "password")
+        fields = ("username", "password")
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, request=None, *args, **kwargs):
+        """
+        The 'request' parameter is set for custom auth use by subclasses.
+        The form data comes in via the standard 'data' kwarg.
+        """
+        self.request = request
+        self.user_cache = None
         super(LoginForm, self).__init__(*args, **kwargs)
-        self.fields['user'].widget.attrs={'class':'form-group form-control',
+
+        self.fields['username'].widget.attrs={'class':'form-group form-control',
         'placeholder':'Username', 'autofocus':''}
         self.fields['password'].widget.attrs={'class':'form-group form-control',
         'placeholder':'Password', 'value':''}
+
+    def clean(self):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+
+        if username and password:
+            self.user_cache = authenticate(username=username,
+                                           password=password)
+            if self.user_cache is None:
+                raise forms.ValidationError(
+                    self.error_messages['invalid_login'],
+                    code='invalid_login',
+                    params={'username': self.username_field.verbose_name},
+                )
+            else:
+                self.confirm_login_allowed(self.user_cache)
+
+        return self.cleaned_data
 
 class AddPostForm(forms.ModelForm):
 
