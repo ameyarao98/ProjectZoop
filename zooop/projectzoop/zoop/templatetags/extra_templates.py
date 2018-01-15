df4c4503340df00c7c9b0b02ae89c426f3223e6d
Ameya Rao <ameyarao98@gmail.com>
2018-01-15 09:31:04 +0100

    a

--- zooop/projectzoop/zoop/templatetags/extra_templates.py
+++ zooop/projectzoop/zoop/templatetags/extra_templates.py
@@ -9,3 +9,13 @@ def get_at_index(list, index):
 @register.filter
 def get_details(object, index):
     return getattr(object, index)
+
+@register.filter
+def get_item(dictionary, key):
+    print(dictionary)
+    return dictionary.get(key)
+
+@register.filter
+def get_count_list(dictionary, postid):
+    count_list = [v for k,v in dictionary.items() if k[0]==postid]
+    return count_list
